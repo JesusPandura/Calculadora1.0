78dd93d1e168a0a2e37634b69d7abc3aeafc6c63
Jesus Pandura <jesmal.flex@hotmail.com>
2020-10-09 03:40:17 -0700

    Calculadora

---
+++ calcula/Clases/Mult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora.Clases
+{
+    class Mult
+    {
+        public double mult(double n1, double n2)
+        {
+            double m;
+            m = n1 * n2;
+            return m;
+
+        }
+    }
+}
---
+++ calcula/Clases/Suma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora.Clases
+{
+    class Suma
+    {
+        public double sumar(double n1, double n2)
+        {
+            double s;
+            s = n1 + n2;
+            return s;
+
+        }
+    }
+}
